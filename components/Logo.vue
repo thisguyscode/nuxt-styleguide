26abff1252c0ff365001f907967b8c960d18dbd4
jamie <jamie@socialcops.com>
2017-11-17 12:16:53 +0000

    Applied @value for path

--- components/Logo.vue
+++ components/Logo.vue
@@ -11,15 +11,19 @@
 
 <style lang="scss" module>
 
+/* Global Style Dependencies
+======================================================================== */
+@value test "sass-loader!~/assets/styles/test.scss";
+
 .container {
-  composes: o-box, o-box--large from "sass-loader!~/assets/styles/test.scss";
-  composes: o-layout, o-layout--large from "sass-loader!~/assets/styles/test.scss";
+  composes: o-box, o-box--large from test;
+  composes: o-layout, o-layout--large from test;
   background-color: black;
 }
 
 .child {
-  composes: o-layout__item from "sass-loader!~/assets/styles/test.scss";
-  composes: u-1/2 from "sass-loader!~/assets/styles/test.scss";
+  composes: o-layout__item from test;
+  composes: u-1/2 from test;
 }
 
 .grandchild {
