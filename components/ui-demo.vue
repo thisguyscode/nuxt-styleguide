0acfbf978f1bae5ee25ef2acd6fdfc04ecacc314
jamie <jamie@socialcops.com>
2017-11-17 16:31:36 +0000

    Continued to flesh out styling structure

--- components/ui-demo.vue
+++ components/ui-demo.vue
@@ -2,13 +2,19 @@
 <!--  -->
 <div :class="$style.base">
   <div :class="$style.child">
-    <div :class="$style.grandchild"></div>
+    <div :class="$style.grandchild">
+      TEXT
+    </div>
   </div>
   <div :class="$style.child">
-    <div :class="$style.grandchild"></div>
+    <div :class="$style.grandchild">
+      TEXT
+    </div>
   </div>
   <div :class="$style.child">
-    <div :class="$style.grandchild"></div>
+    <div :class="$style.grandchild">
+      TEXT
+    </div>
   </div>
   <code :class="$style.code">
     <pre>{{ code }}</pre>
@@ -33,6 +39,7 @@ export default {
 /* Dependencies
 ========================================================================== */
 @value o-box "sass-loader!~/assets/styles/objects/objects.box.scss";
+@value o-button "sass-loader!~/assets/styles/objects/objects.button.scss";
 @value o-layout "sass-loader!~/assets/styles/objects/objects.layout.scss";
 
 @value c-background-color "sass-loader!~/assets/styles/cosmetics/cosmetics.background-color.scss";
@@ -46,9 +53,9 @@ export default {
 /* Base class
 ========================================================================== */
 .base {
-  composes: layout  layout--large  from  o-layout;
-  composes: orange  from  c-background-color;
-  composes: box  from  o-box;
+  composes: layout  layout--large  from o-layout;
+  composes: orange  from c-background-color;
+  composes: box  from o-box;
 }
 
 
@@ -56,13 +63,13 @@ export default {
 /* Child classes
 ========================================================================== */
 .child {
-  composes: layout__item  from  o-layout;
-  composes: width-1/2  width-1/3-at-desktop  from  u-widths;
+  composes: layout__item  from o-layout;
+  composes: width-1/2  width-1/3-at-desktop  from u-widths;
 }
 
 .grandchild {
-  composes: button  from  c-button;
-  height: 100px;
+  composes: button  from o-button;
+  composes: button  from c-button;
 }
 
 .code {
