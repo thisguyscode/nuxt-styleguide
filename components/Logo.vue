845e2efe94d13912fe5b0f30b46db8971d33af76
jamie <jamie@socialcops.com>
2017-11-17 12:11:15 +0000

    Comandered logo component for style tests

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,79 +1,30 @@
 <template>
-  <div class="VueToNuxtLogo">
-    <div class="Triangle Triangle--two"></div>
-    <div class="Triangle Triangle--one"></div>
-    <div class="Triangle Triangle--three"></div>
-    <div class="Triangle Triangle--four"></div>
+  <div :class="$style.container">
+    <div :class="$style.child">
+      <div :class="$style.grandchild"></div>
+    </div>
+    <div :class="$style.child">
+      <div :class="$style.grandchild"></div>
+    </div>
   </div>
 </template>
 
-<style>
-.VueToNuxtLogo {
-  display: inline-block;
-  animation: turn 2s linear forwards 1s;
-  transform: rotateX(180deg);
-  position: relative;
-  overflow: hidden;
-  height: 180px;
-  width: 245px;
+<style lang="scss" module>
+
+.container {
+  composes: o-box, o-box--large from "sass-loader!~/assets/styles/test.scss";
+  composes: o-layout, o-layout--large from "sass-loader!~/assets/styles/test.scss";
+  background-color: black;
 }
 
-.Triangle {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 0;
-  height: 0;
+.child {
+  composes: o-layout__item from "sass-loader!~/assets/styles/test.scss";
+  composes: u-1/2 from "sass-loader!~/assets/styles/test.scss";
 }
 
-.Triangle--one {
-  border-left: 105px solid transparent;
-  border-right: 105px solid transparent;
-  border-bottom: 180px solid #41B883;
+.grandchild {
+  height: 100px;
+  background-color: blue;
 }
 
-.Triangle--two {
-  top: 30px;
-  left: 35px;
-  animation: goright 0.5s linear forwards 3.5s;
-  border-left: 87.5px solid transparent;
-  border-right: 87.5px solid transparent;
-  border-bottom: 150px solid #3B8070;
-}
-
-.Triangle--three {
-  top: 60px;
-  left: 35px;
-  animation: goright 0.5s linear forwards 3.5s;
-  border-left: 70px solid transparent;
-  border-right: 70px solid transparent;
-  border-bottom: 120px solid #35495E;
-}
-
-.Triangle--four {
-  top: 120px;
-  left: 70px;
-  animation: godown 0.5s linear forwards 3s;
-  border-left: 35px solid transparent;
-  border-right: 35px solid transparent;
-  border-bottom: 60px solid #fff;
-}
-
-@keyframes turn {
-  100% {
-    transform: rotateX(0deg);
-  }
-}
-
-@keyframes godown {
-  100% {
-    top: 180px;
-  }
-}
-
-@keyframes goright {
-  100% {
-    left: 70px;
-  }
-}
 </style>
