9a167aa18e250dfad747e83cad39dcb550866f43
jamie <jamie@socialcops.com>
2017-11-20 13:05:53 +0000

    Included more icons from vue-awesome

--- plugins/vue-awesome.js
+++ plugins/vue-awesome.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import Icon from 'vue-awesome/components/Icon'
 
+import 'vue-awesome/icons/home'
+
 import 'vue-awesome/icons/folder'
 import 'vue-awesome/icons/folder-o'
 import 'vue-awesome/icons/file'
 import 'vue-awesome/icons/file-o'
+
+import 'vue-awesome/icons/chevron-right'
 
 // Registered globally as ui-icon
 Vue.component('ui-icon', Icon)
