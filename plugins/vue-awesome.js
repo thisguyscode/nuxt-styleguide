67c40ed4171dbe172f3cc337857620d57378cb10
jamie <jamie@socialcops.com>
2017-11-22 12:04:01 +0000

    Added required icons

--- plugins/vue-awesome.js
+++ plugins/vue-awesome.js
@@ -1,14 +1,16 @@
 import Vue from 'vue'
 import Icon from 'vue-awesome/components/Icon'
 
+// DIRECTORY
 import 'vue-awesome/icons/home'
-
 import 'vue-awesome/icons/folder'
 import 'vue-awesome/icons/folder-open-o'
 import 'vue-awesome/icons/file'
 import 'vue-awesome/icons/file-o'
-
 import 'vue-awesome/icons/chevron-right'
+
+// MISC
+import 'vue-awesome/icons/eye'
 
 // Registered globally as ui-icon
 Vue.component('ui-icon', Icon)
