e32cad424808fe63fb449fad3f6055bbd2ea3404
jamie <jamie@socialcops.com>
2017-11-22 12:36:57 +0000

    Added code (via raw-loader) to file pages

--- pages/home/common/_category/_file.vue
+++ pages/home/common/_category/_file.vue
@@ -1,15 +1,46 @@
 <template>
   <section>
-    <h1>{{ $route.params.file }}</h1>
+    <h1 :class="$style.heading">{{ $route.params.file }}</h1>
+    <code>
+      <pre>{{ code }}</pre>
+    </code>
   </section>
 </template>
 
 <script>
 export default {
-
+  computed: {
+    code: function () {
+      // if (this.$route.params.file && this.$route.params.category) {
+      return require(`!raw-loader!~/assets/styles/${this.$route.params.category}/${this.$route.params.file}`)
+      // }
+    }
+  }
 }
 </script>
 
-<style>
+
+<style lang="scss" module>
+
+/* Dependencies
+========================================================================== */
+@value o-heading "sass-loader!~/assets/styles/objects/objects.heading.scss";
+
+
+/* Root class
+========================================================================== */
+.root {
+  //
+}
+
+
+
+/* ContentArea
+========================================================================== */
+
+  .heading {
+    composes: heading heading--alpha  from o-heading;
+  }
 
 </style>
+
