eb99aa8add5a68c39ce288adb424324ce0c4fa8f
jamie <jamie@socialcops.com>
2017-11-24 18:34:40 +0000

    Created all-components from scripts

---
+++ plugins/all-components.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+
+import uiA from '~/components/ui-a.vue'
+import uiAExample from '~/components/ui-a.vue/example'
+
+import uiBreadcrumbs from '~/components/ui-breadcrumbs.vue'
+import uiBreadcrumbsExample from '~/components/ui-breadcrumbs.vue/example'
+
+import uiCodeBlock from '~/components/ui-code-block.vue'
+import uiCodeBlockExample from '~/components/ui-code-block.vue/example'
+
+import uiDirectoryItem from '~/components/ui-directory-item.vue'
+import uiDirectoryItemExample from '~/components/ui-directory-item.vue/example'
+
+import uiDirectoryTree from '~/components/ui-directory-tree.vue'
+import uiDirectoryTreeExample from '~/components/ui-directory-tree.vue/example'
+
+import uiSidebar from '~/components/ui-sidebar.vue'
+import uiSidebarExample from '~/components/ui-sidebar.vue/example'
+
+Vue.component('ui-a', uiA)
+Vue.component('ui-a-example', uiAExample)
+
+Vue.component('ui-breadcrumbs', uiBreadcrumbs)
+Vue.component('ui-breadcrumbs-example', uiBreadcrumbsExample)
+
+Vue.component('ui-code-block', uiCodeBlock)
+Vue.component('ui-code-block-example', uiCodeBlockExample)
+
+Vue.component('ui-directory-item', uiDirectoryItem)
+Vue.component('ui-directory-item-example', uiDirectoryItemExample)
+
+Vue.component('ui-directory-tree', uiDirectoryTree)
+Vue.component('ui-directory-tree-example', uiDirectoryTreeExample)
+
+Vue.component('ui-sidebar', uiSidebar)
+Vue.component('ui-sidebar-example', uiSidebarExample)
