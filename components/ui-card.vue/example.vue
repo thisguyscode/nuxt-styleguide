8b117c89f24233ea26c322e0a190a75bd002fbe3
jamie <jamie@socialcops.com>
2017-11-25 11:09:14 +0000

    Created ui-card component

---
+++ components/ui-card.vue/example.vue
@@ -0,0 +1,21 @@
+<template>
+  <ui-card>
+    <p :class="$style.text">Text</p>
+  </ui-card>
+</template>
+
+
+<script>
+export default {}
+</script>
+
+
+<style lang="scss" module>
+
+@value o-text "sass-loader!~/assets/styles/objects/objects.text.scss";
+
+.text {
+  composes: text  from o-text;
+}
+
+</style>
