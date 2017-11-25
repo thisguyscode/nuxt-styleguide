cec45e979958a521ecbd8b15580d29450534f7a5
jamie <jamie@socialcops.com>
2017-11-25 13:06:24 +0000

    Added contrast prop to ui-card

--- components/ui-card.vue/script.js
+++ components/ui-card.vue/script.js
@@ -1,3 +1,21 @@
 export default {
-  //
+  props: {
+    contrast: {
+      type: String,
+      default: 'normal'
+    }
+  },
+  computed: {
+    rootClass: function () {
+      var array = []
+      if (this.contrast === 'heavy') {
+        array.push(this.$style.root_contrastHeavy)
+      } else if (this.contrast === 'light') {
+        array.push(this.$style.root_contrastLight)
+      } else {
+        array.push(this.$style.root_contrastNormal)
+      }
+      return array
+    }
+  }
 }
