8478bd9cfc6c793328162aaba717ea59fdcb8c18
jamie <jamie@socialcops.com>
2017-11-25 18:24:17 +0000

    Fleshed out mini-menu component

--- components/ui-mini-menu.vue/example.vue
+++ components/ui-mini-menu.vue/example.vue
@@ -1,7 +1,29 @@
 <template>
-  <ui-mini-menu></ui-mini-menu>
+  <ui-mini-menu>
+    <li @click="act">
+      <ui-icon name="eye"/>
+      <span>Text</span>
+    </li>
+  </ui-mini-menu>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    menuList: function () {
+      var array = []
+      for (var i = 0; i < this.menuItems.length; i++) {
+        var current = this.menuItems[i]
+        current.action = this.act()
+        array.push(current)
+      }
+      return array
+    }
+  },
+  methods: {
+    act: function () {
+      console.log('abc')
+    }
+  }
+}
 </script>
